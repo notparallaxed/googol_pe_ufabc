668e13c351279a47d68325566183dc01abbef2f8
Vinicius Teixeira <viniciusmqueiroga@gmail.com>
2018-11-09 02:07:39 -0200

    Renamed tamNumerao to sanitizeNumerao and add some fuctions

--- googol-part1.c
+++ googol-part1.c
@@ -8,14 +8,15 @@
 
 #define MAX_SIZE 1003
 
-int tamNumerao(char numerao[MAX_SIZE]);
+void imprimeNumerao(char numerao[MAX_SIZE], int tam);
+int sanitizeNumerao(char numerao[MAX_SIZE]);
 
 /* return tamNumeraoResultado */
 int soma(char numerao1[MAX_SIZE], int n1, char numerao2[MAX_SIZE], int n2, char *numeraoResultado);
 
 int main() {
     char numerao1[MAX_SIZE], numerao2[MAX_SIZE], operador = ' ', *numeraoResultado; 
-    int tam1 = 0, tam2 = 0, tam3 = 0
+    int tam1 = 0, tam2 = 0, tam3 = 0;
     
     while(operador != '0'){
         /* primeira leitura (numero ou fim) */
@@ -25,8 +26,8 @@ int main() {
             scanf("%s", numerao2);
             scanf(" %c", &operador);
         
-            tam1 = tamNumerao(numerao1);
-            tam2 = tamNumerao(numerao2);
+            tam1 = sanitizeNumerao(numerao1);
+            tam2 = sanitizeNumerao(numerao2);
             
             switch(operador){
                 case '+': tam3 = soma(numerao1, tam1, numerao2, tam2, numeraoResultado);
@@ -34,7 +35,7 @@ int main() {
                 default: printf("Operador invalido!");
             }
             
-            printf("%s", numeraoResultado);
+            imprimeNumerao(numeraoResultado, tam3);
             
         } else {
             /* define operador como zero para encerrar aplicacao */
@@ -45,14 +46,45 @@ int main() {
     return 0;
 }
 
-int tamNumerao(char numerao[MAX_SIZE]){
-    int i = 0;
+void imprimeNumerao(char numerao[MAX_SIZE], int tam) {
+    int i;
+    for(i = 0; i < tam; i++) {
+        printf("%c", numerao[i]);
+    }
+}
 
-    while(i < MAX_SIZE) {
+int sanitizeNumerao(char numerao[MAX_SIZE]) {
+    char numerao_copy[MAX_SIZE];
+    int i = 0, size = -1;
+    
+    /* encontra o tamanho do numerao */
+    while(size == -1 && i < MAX_SIZE) {
         if(numerao[i] == '\0'){
-           return i;
+           size = i;
         }
         i++;
     }
-    return 0;  
+    
+    /* cria um espelho */
+    for(i = 0; i < size; i++) {
+        numerao_copy[i] = numerao[i];
+    }
+    
+    /* reordena do menos significativo para o mais significativo */
+    if(numerao[0] == '-') {
+        /* mantem o menos para simbolizar o numero negativo */
+        for(i = 1; i < size; i++){
+            numerao[i] = numerao_copy[size - i];
+        }
+    } else {
+        for(i = 0; i < size; i++){
+            numerao[i] = numerao_copy[(size - 1) - i];
+        }
+    }
+        
+    return size;  
+}
+
+int soma(char numerao1[MAX_SIZE], int tam1, char numerao2[MAX_SIZE], int tam2, char *numeraoResultado) {
+    /*falta implementar essa bagaça aqui*/
 }
